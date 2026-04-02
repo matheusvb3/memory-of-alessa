3c24bbd562f05c0aab476367bd77212e3dd499c0
Daniel Freitas <101065507+Dandiels@users.noreply.github.com>
2026-04-01 21:55:37 -0300

    sh3: match func_01F6D720_subway_01 & func_01F6E2D0_subway_01 (#89)
    * update subway_01.h
    
    * match func_01F6D720_subway_01 & func_01F6E2D0_subway_01

--- silent-hill-3/src/Event/subway_01.c
+++ silent-hill-3/src/Event/subway_01.c
@@ -28,7 +28,138 @@ int func_01F6D690_subway_01(void)
     return var_s0;
 }
 
-INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6D720_subway_01);
+int func_01F6D720_subway_01(void)
+{
+    float sp38;
+    float sp3C;
+    float var_f12;
+    float temp_f12;
+    float temp_f13;
+    float temp_f0;
+    SubCharacter* temp_v0;
+    int var_s0;
+    SubCharacter* var_v0;
+    short temp_v1;
+
+    var_s0 = 0;
+    temp_v1 = RoomName();
+    func_001908A0(&sp38, &sp3C);
+    
+    switch (temp_v1)
+    {
+        case 0x39:
+            if (sp3C > 60000.0f)
+            {
+                D_01F70380_subway_01 = 6;
+                D_01F70384_subway_01 = 7;
+            }
+            else
+            {
+                D_01F70380_subway_01 = 0xB;
+                D_01F70384_subway_01 = 0xA;
+            }
+            
+            var_v0 = shCharacterGetSubCharacter(0x1009, 0x6C);
+            break;
+        case 0x3A:
+            D_01F70380_subway_01 = 8;
+            D_01F70384_subway_01 = 7;
+            var_v0 = shCharacterGetSubCharacter(0x1009, 0x6E);
+            break;
+        case 0x3B:
+            if (sp3C > 20000.0f)
+            {
+                D_01F70380_subway_01 = 9;
+                D_01F70384_subway_01 = 0xA;
+            }
+            else
+            {
+                D_01F70380_subway_01 = 0xC;
+                D_01F70384_subway_01 = 0xD;
+            }
+            
+            var_v0 = shCharacterGetSubCharacter(0x1009, 0x70);
+            break;
+        default:
+            return 1;
+    }
+
+    if (var_v0 != 0)
+    {
+        func_001DC9E0(var_v0, 0);
+    }
+    
+    switch (D_01F70700_subway_01)
+    {
+        case 0:
+            func_00190A20(2);
+            func_001C2290(3, 0.5f);
+            temp_f12 = 1.0f;
+            temp_f13 = 0.0f;
+            SeCall(temp_f12, temp_f13, 0x3139);
+            func_01F6E2D0_subway_01();
+            func_0013D250(0, &D_01F70180_subway_01, 1.0f);
+            D_01F70700_subway_01 = 1;
+            break;
+        case 1:
+        case 2:
+            if (func_0016C540(&D_01F70380_subway_01, &D_01F703E0_subway_01) == 0)
+            {
+                temp_f0 = func_001643C0();
+
+                if ((D_01F70700_subway_01 == 1) && (temp_f0 > 145.0f))
+                {
+                    func_0013D250(0, &D_01F70140_subway_01, 1.0f);
+                    D_01F70700_subway_01 = 2;
+                }
+                
+                if (func_001646C0() != 0)
+                {
+                    if (func_00151150(0, 1) != 0)
+                    {
+                        var_f12 = 0.8f;
+                    }
+                    else
+                    {
+                        var_f12 = 1.2f;
+                    }
+                    
+                    func_001C2290(3, var_f12);
+                }
+            }
+            else
+            {
+                func_0019A940();
+                D_01F70700_subway_01 = 3;
+            }
+            
+            break;
+        case 3:
+            if ((func_0019A9B0(2.0f) != 0) || (func_001646C0() != 0))
+            {
+                var_s0 = 1;
+            }
+            
+            break;
+    }
+    
+    temp_v0 = shCharacterGetSubCharacter(HEATHER_CHARA_ID, -1);
+    
+    if (var_s0 != 0)
+    {
+        func_001DC9E0(temp_v0, 1);
+        func_00190C40();
+        func_00190A20(0);
+        func_0013D280(0);
+        D_01F70700_subway_01 = 0;
+    }
+    else
+    {
+        func_001DC9E0(temp_v0, 0);
+    }
+    
+    return var_s0;
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6DAA0_subway_01);
 
@@ -54,7 +185,18 @@ void func_01F6E270_subway_01(void)
     }
 }
 
-INCLUDE_ASM("asm/nonmatchings/Event/subway_01", func_01F6E2D0_subway_01);
+void func_01F6E2D0_subway_01(void)
+{
+    SubCharacter* var_v0;
+
+    var_v0 = shCharacterGetSubCharacter(DOUBLE_HEAD_CHARA_ID, -1);
+
+    while (var_v0 != NULL)
+    {
+        shCharacter_Manage_Delete(DOUBLE_HEAD_CHARA_ID, var_v0->id);
+        var_v0 = shCharacterGetSubCharacter(DOUBLE_HEAD_CHARA_ID, -1);
+    }
+}
 
 void func_01F6E320_subway_01(void)
 {
--- silent-hill-3/src/Event/subway_01.h
+++ silent-hill-3/src/Event/subway_01.h
@@ -4,16 +4,35 @@
 #include "common.h"
 #include "Chacter/m3_sc.h"
 
-int func_0016CED0(int, int, int*);
 int func_00190A20(int);
+int func_0016CED0(int, int, int*);
 short RoomName(void);
+void func_001908A0(float *, float *);
+SubCharacter* shCharacterGetSubCharacter(int, short);
+void func_001DC9E0(SubCharacter *, int);
+void func_001C2290(int, float);
+int SeCall(float, float, int);
+void func_01F6E2D0_subway_01(void);
+void func_0013D250(int, int *, float);
+int func_0016C540(int *, int *);
+float func_001643C0(void);
+int func_001646C0(void);
+int func_00151150(int, int);
+void func_0019A940(void);
+int func_0019A9B0(float);
+void func_00190C40(void);
+void func_0013D280(int);
 void func_01F6E7C0_subway_01(void);
 
-extern int D_01F70370_subway_01;
 extern int D_01F70700_subway_01;
+extern int D_01F70370_subway_01;
+extern int D_01F70380_subway_01;
+extern int D_01F70384_subway_01;
+extern int D_01F70180_subway_01;
+extern int D_01F703E0_subway_01;
+extern int D_01F70140_subway_01;
 extern int D_01F70650_subway_01;
 extern int D_1D3165C;
-extern int D_01F70700_subway_01;
 extern int D_01F70708_subway_01;
 extern int D_01F70718_subway_01;
 
